9291472a6ef60ce7f6bc78b80fb435c01d6b6b8e
Brian McClune <bpmcclune@gmail.com>
2021-10-17 09:23:33 -0400

    Update test to avoid use of real router

--- src/App.spec.ts
+++ src/App.spec.ts
@@ -1,23 +1,13 @@
 import { mount } from '@vue/test-utils'
-import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
 
-const router = createRouter({
-  history: createWebHistory(),
-  routes: [{
-    path: '/',
-    component: {
-      template: '<div>O kaeri nasai</div>'
-    }
-  }]
-})
-
 test('App uses router-view', async () => {
-  router.push('/')
-  await router.isReady()
-
   const wrapper = mount(App, {
-    global: { plugins: [router] }
+    global: {
+      stubs: {
+        'router-view': { template: 'O kaeri nasai'}
+      }
+    }
   })
-  expect(wrapper.html()).toContain('O kaeri nasai')
+  expect(wrapper.html()).toBe('O kaeri nasai')
 })
